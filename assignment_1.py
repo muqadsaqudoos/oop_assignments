cf09ab37e61e6c9a0dd687a638e442c4b48f2dce
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2023-09-23 11:43:43 -0700

    assignment 1

---
+++ assignment_1.py
@@ -0,0 +1,30 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+class Fraction:
+    def __init__(self,num,den):
+        self.num = num
+        self.den = den
+        
+    def show(self):
+        print(f"{self.num}/{self.den}")
+        
+    def multiply(self,other):
+        num = self.num*other.num
+        den = self.den*other.den
+        return f"{num}/{den}"
+    
+    def real_number(self):
+        return (self.num)/(self.den)
+    
+
+def main():
+    a = Fraction(3,4)
+    a.show()
+    b= Fraction(9,10)
+    f = a.multiply(b)
+    print(f)
+    
+    print(b.real_number())
+
+main()
+
